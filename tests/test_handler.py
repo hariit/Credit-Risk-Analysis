d8a1681d2011d2d4681191ee09995c802720dc55
Hariharan Mani <hariharan.it@gmail.com>
2020-01-23 11:16:27 +0800

    path changed
    path changed

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,17 +1,15 @@
 import unittest
 import index
 
-
 class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
         print("testing response.")
-        result = index.handler(None, None)
+        result = index.handler({'queryStringParameters': {'string': 'Karsten'}}, None)
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('Hello World', result['body'])
-
+        self.assertIn(False, result['body'])
 
 if __name__ == '__main__':
     unittest.main()
